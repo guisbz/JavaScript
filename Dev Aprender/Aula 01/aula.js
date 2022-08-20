5b7751a7fcaad0012c8ae527fbcca3e09891c47c
guisbz <109392433+guisbz@users.noreply.github.com>
2022-08-20 18:49:40 -0300

    Initial commit

---
+++ Dev Aprender/Aula 01/aula.js
@@ -0,0 +1,8 @@
+const nome = 2; // const é usado para variáveis que não vão se alterar durante o código, já let é usado caso vc venha a fazer alguma alteração.
+let idade = 1;
+let altura = 0;
+console.log(idade);
+
+let nNome = 'Jabison'; //String literal
+let iIdade = 25; // number literal
+let estaAprovado = true; //boolean
